a2976fa933f494b7f234ed8e4d0f131ab8f25bb6
Juan E. Sanchez <juan@tcad.com>
2019-06-17 22:16:31 -0500

    more work on refinement

--- refinement/run.sh
+++ refinement/run.sh
@@ -1,3 +1,11 @@
-
+#!/bin/bash
+set -e
 gmsh -2 -format 'msh2' gmsh_mos2d.geo
+python mos2d_refine.py
+#gmsh -2 bjt.geo -bgm ./bjt_bgmesh.pos
+#python bjt_refine.py
+#gmsh -2 bjt.geo -bgm ./bjt_bgmesh.pos
+#python bjt_refine.py
+#gmsh -2 bjt.geo -bgm ./bjt_bgmesh.pos
+#python bjt_refine.py
 
