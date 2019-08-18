738eee14daabb51c0365b9c892299cbf3dbf8653
Juan E. Sanchez <juan@tcad.com>
2019-08-18 18:59:49 -0500

    save mesh

--- refinement/run.sh
+++ refinement/run.sh
@@ -8,7 +8,7 @@ python mos2d_refine.py
 #python mos2d_refine.py
 for i in 1, 2, 3, 4, 5, 6, 7, 8, 9, 10; do 
 #for i in 1, 2, 3; do 
-#gmsh -2 -format 'msh2' -bgm ./bgmesh.pos  gmsh_mos2d.geo
-gmsh -2 -format 'msh2' -bgm ./bgmesh.pos -merge gmsh_mos2d.geo
+gmsh -2 -format 'msh2' -bgm ./bgmesh.pos  gmsh_mos2d.geo
+#gmsh -2 -format 'msh2' -bgm ./bgmesh.pos -merge gmsh_mos2d.geo
 python mos2d_refine.py
 done
